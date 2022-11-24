23462aa61ee4488e6f47148c0950d09b2ee2a4c7
cql <CaoQiaolian@zjhqtech.com>
2022-11-24 09:31:01 +0800

    定制修改

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -181,7 +181,7 @@ detect_mac80211() {
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=Hiwooya
+			set wireless.default_radio${devidx}.ssid=Kitesoft_$(cat /sys/class/ieee80211/${dev}/macaddress|awk -F ":" '{print $4""$5""$6 }'| tr a-z A-Z)
 			set wireless.default_radio${devidx}.encryption=psk2
 			set wireless.default_radio${devidx}.key=1234567890
 EOF
