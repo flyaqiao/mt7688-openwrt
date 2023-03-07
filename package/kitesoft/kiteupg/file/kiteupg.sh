114f1afd8fba19c128fae8fcee51d3a6d31488af
cql <CaoQiaolian@zjhqtech.com>
2023-03-07 14:47:00 +0800

    增加自动NFS升级KITEDTU功能

---
+++ package/kitesoft/kiteupg/file/kiteupg.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+mkdir -p /mnt/nfs
+mount -o nolock ssh.kitesoft.cn:/home/flyaqiao/mt7688/openwrt-21.02/bin/packages/mipsel_24kc/base /mnt/nfs
+opkg remove kitedtu
+opkg install /mnt/nfs/kitedtu_1.0-1_mipsel_24kc.ipk
