0b37430c9558fff5ae8fa1c36463c25146b7fc19
cql <CaoQiaolian@zjhqtech.com>
2023-03-17 15:18:38 +0800

    串口自动打开USB1和USB2

--- package/kitesoft/kitedtu/src/atport.c
+++ package/kitesoft/kitedtu/src/atport.c
@@ -17,18 +17,77 @@
 
 static MUTEX_T m_AtportMutex;
 #define DEFAULT_TIMEOUT 3000
-static int serial_fd = -1;
 static int ccid_ok = 0;
 static int location_ok = 0;
 static int csq = 0;
+static int OpenCom()
+{
+  int serial_fd;
+  struct termios old_cfg;
+  struct termios new_cfg;
+  char szComm[32];
+  sprintf(szComm, "/dev/ttyUSB1");
+  //1.打开串口设备文件
+  serial_fd = open(szComm, O_RDWR | O_NOCTTY | O_NDELAY);
+  if (serial_fd == -1) {
+    sprintf(szComm, "/dev/ttyUSB2");
+    serial_fd = open(szComm, O_RDWR | O_NOCTTY | O_NDELAY);
+    if (serial_fd == -1) {
+      printf("open fail\n");
+      return -1;
+    }
+  }
+  //2.备份现有的串口配置
+  if (-1 == tcgetattr(serial_fd, &old_cfg)) {
+    printf("tcgetattr\n");
+    return -2;
+  }
+  //3.原始模式
+  new_cfg = old_cfg;
+  cfmakeraw(&new_cfg);
+  //4.配置波特率
+  //cfsetispeed(&new_cfg, B115200);
+  //cfsetospeed(&new_cfg, B115200);
+  cfsetspeed(&new_cfg, B115200);
+  //5.设置控制标志
+  new_cfg.c_cflag |= CREAD | CLOCAL;//使能数据接收和本地模式
+  /* struct termio newtio; */
+  new_cfg.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); /*Input*/
+  new_cfg.c_oflag &= ~OPOST; /*Output*/
+  //6.设置帧结构
+  new_cfg.c_cflag &= ~CSTOPB;//1位停止位
+  new_cfg.c_cflag &= ~CSIZE;//去掉数据位屏蔽
+  new_cfg.c_cflag |= CS8;//8位数据位
+  new_cfg.c_cflag &= ~PARENB;//无校验
+  //7.设置阻塞模式
+  //fcntl(serial_fd, F_SETFL, 0); //设为阻塞
+  //收到1字节解除阻塞
+  new_cfg.c_cc[VTIME] = 0;
+  new_cfg.c_cc[VMIN] = 0;
+  tcflush(serial_fd, TCIFLUSH);
+  //8.使能配置生效
+  if (-1 == tcsetattr(serial_fd, TCSANOW, &new_cfg)) {
+    printf("tcgetattr\n");
+    return -3;
+  }
+  return serial_fd;
+}
+static void CloseCom(int serial_fd)
+{
+  close(serial_fd);
+}
 int at_send_printf(char *resp, int size, const char *fmt, ...)
 {
   char line[128];                                    //发送行
   int ret = 0;
-  if (serial_fd == -1)
-    return 0;
+  int serial_fd;
   va_list args;
   Lock(&m_AtportMutex);
+  serial_fd = OpenCom();
+  if (serial_fd < 0) {
+    Unlock(&m_AtportMutex);
+    return -3;
+  }
   tcflush(serial_fd, TCIOFLUSH);
   va_start(args, fmt);
   vsnprintf(line, sizeof(line), fmt, args);
@@ -54,6 +113,7 @@ int at_send_printf(char *resp, int size, const char *fmt, ...)
       }
     }
   }
+  CloseCom(serial_fd);
   Unlock(&m_AtportMutex);
   return ret;
 }
@@ -96,65 +156,6 @@ int GprsGetCsq()
 }
 void GprsInit()
 {
-  struct termios old_cfg;
-  struct termios new_cfg;
-  char szComm[32];
   OpenLock(&m_AtportMutex);
-  Lock(&m_AtportMutex);
-  sprintf(szComm, "/dev/ttyUSB1");
-  //1.打开串口设备文件
-  serial_fd = open(szComm, O_RDWR | O_NOCTTY | O_NDELAY);
-  if (serial_fd == -1) {
-    printf("open fail");
-    return;
-  }
-  //2.备份现有的串口配置
-  if (-1 == tcgetattr(serial_fd, &old_cfg)) {
-    printf("tcgetattr");
-    return;
-  }
-  //3.原始模式
-  new_cfg = old_cfg;
-  cfmakeraw(&new_cfg);
-  //4.配置波特率
-  //cfsetispeed(&new_cfg, B115200);
-  //cfsetospeed(&new_cfg, B115200);
-  cfsetspeed(&new_cfg, B115200);
-  //5.设置控制标志
-  new_cfg.c_cflag |= CREAD | CLOCAL;//使能数据接收和本地模式
-  /* struct termio newtio; */
-  new_cfg.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); /*Input*/
-  new_cfg.c_oflag &= ~OPOST; /*Output*/
-  //6.设置帧结构
-  new_cfg.c_cflag &= ~CSTOPB;//1位停止位
-  new_cfg.c_cflag &= ~CSIZE;//去掉数据位屏蔽
-  new_cfg.c_cflag |= CS8;//8位数据位
-  new_cfg.c_cflag &= ~PARENB;//无校验
-  //7.设置阻塞模式
-  //fcntl(serial_fd, F_SETFL, 0); //设为阻塞
-  //收到1字节解除阻塞
-  new_cfg.c_cc[VTIME] = 0;
-  new_cfg.c_cc[VMIN] = 0;
-  tcflush(serial_fd, TCIFLUSH);
-  //8.使能配置生效
-  if (-1 == tcsetattr(serial_fd, TCSANOW, &new_cfg)) {
-    printf("tcgetattr");
-    return;
-  }
-  Unlock(&m_AtportMutex);
   at_send_printf(NULL, 0, "ATE0\r\n");
-#if 0
-  while (1) {
-    memset(szComm, 0, sizeof(szComm));
-    int len = read(serial_fd, szComm, sizeof(szComm) - 1);
-    if (len > 0)
-      printf("%s", szComm); /* 打印从串口读出的字符串 */
-  }
-  //恢复配置
-  if (-1 == tcsetattr(serial_fd, TCSANOW, &old_cfg)) {
-    printf("tcgetattr");
-    return;
-  }
-  close(serial_fd);
-#endif
 }
