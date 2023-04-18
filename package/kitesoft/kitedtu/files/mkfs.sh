ae75c2b66c6db10ea6e30ab9d6609736b24e7c35
cql <CaoQiaolian@zjhqtech.com>
2023-04-18 12:56:30 +0800

    web_fs编译时生成

--- package/kitesoft/kitedtu/files/mkfs.sh
+++ package/kitesoft/kitedtu/files/mkfs.sh
@@ -1,3 +1,10 @@
+# 判断是否软链接,如果是则切换到build.sh实际目录执行
+if [ -L "$0" ]; then
+    pushd $(dirname $(readlink $0))
+else
+    pushd $(dirname $0)
+fi
+
 rm -rf tmp && mkdir tmp && cp -r web_root tmp/
 #find tmp -type f | xargs -n1 gzip
 gcc pack.c -o pack
@@ -6,3 +13,4 @@ cd ..
 rm -rf tmp
 rm pack
 
+popd
